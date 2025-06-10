7a8a290781a3a985d02995fef0a4129b35bf7dd1
judegiordano <judegiordano@gmail.com>
2025-06-10 15:28:04 -0400

    walkdir

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use actions_toolkit::core::{self, Core};
 use anyhow::Result;
-use std::{
-    path::Path,
-    time::{Duration, Instant},
-};
+use std::{path::Path, time::Instant};
+use walkdir::WalkDir;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // std::env::var("GITHUB_WORKSPACE")
     let start = Instant::now();
     let mut core = Core::new();
 
     let name = core::input("name")?;
     core.debug(&format!("Hello, {}!", name))?;
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
     //
     let workspace = std::env::var("GITHUB_WORKSPACE")?;
     let file_path = Path::new(&workspace);
-    let dirs = file_path.ancestors();
-    core.debug(&format!("[DIRS]: [{}]", dirs.count()))?;
+    let entries = WalkDir::new(file_path).follow_links(true).into_iter();
+    for entry in entries {
+        match entry {
+            Ok(entry) => {
+                core.debug(&format!("Found entry: {:?}", entry.path()))?;
+            }
+            Err(e) => {
+                core.debug(&format!("Error reading entry: {}", e))?;
+            }
+        }
+    }
     //
-
     core.set_output("time", format!("{:?}", start.elapsed()))?;
     Ok(())
 }
